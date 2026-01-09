e8cb5a2afcd55201f84b42c247b11ccb8df26d98
johnson2123 <johnsonvasupalli@yahoo.com>
2026-01-09 11:08:49 +0530

    added changes to products

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
   return (
     <>
     <Products/>
-    <SimpleForm/>
     </>
   )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -27,29 +27,39 @@ export default function Products() {
       {loading ? (
         <p className="text-center text-lg">Loading products...</p>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 text-center">
           {products.map((product) => (
             <div
               key={product.id}
               className="bg-white rounded-lg shadow-md p-4 hover:shadow-xl transition"
             >
-              <img
-                src={product.image}
-                alt={product.title}
-                className="h-40 w-full object-contain mb-4"
-              />
+              
+              <div className="group relative">
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  className="h-40 w-full object-contain mb-4"
+                />
+                <p className="absolute top-0 right-0 border-2 px-2 border-hidden text-2xl items-center bg-amber-500 rounded-[50%]">{product.id}</p>
+                <h2 className="text-center text-2xl opacity-0 transition-opacity duration-300 group-hover:opacity-100 font-semibold mb-2 line-clamp-2">
+                  {product.title}
+                </h2>
+              </div>
 
-              <h2 className="font-semibold text-sm mb-2 line-clamp-2">
-                {product.title}
-              </h2>
+              <div className="flex justify-center gap-5 items-center">
+                <p className="text-green-600 text-2xl font-bold mb-2">
+                  ₹ {product.price}
+                </p>
+                <p className="text-yellow-600 text-2xl font-bold mb-2">
+                  ⭐: {product.rating.rate}
+                </p>
+              </div>
 
-              <p className="text-green-600 font-bold mb-2">
-                ₹ {product.price}
-              </p>
-
-              <button className="w-full bg-black text-white py-2 rounded hover:bg-gray-800">
+              <button className=" bg-blue-900 text-white py-2 px-3 rounded hover:bg-gray-800">
                 Add to Cart
               </button>
+
+
             </div>
           ))}
         </div>
